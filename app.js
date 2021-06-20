10ca49d2ec04ac305d10a45d5ca16abada7895ea
ehab-ahma <ehab24495@gmail.com>
2021-06-20 23:39:52 +0300

    added a new file

---
+++ app.js
@@ -0,0 +1,22 @@
+let first = prompt("welcome, do you like cars")
+alert('your answer ' +  first )
+ console.log(first);
+ document.write('<h2>' + 'welcome, doyou like cars' + '</h2>')
+ document.write('<h4>' + first + '</h4>')
+ let second = prompt("can you tell us which type of cars you prefer")
+ alert('your answer '+ second)
+ console.log(second);
+ document.write('<h2>' + 'can you tell us which type of cars you prefer' + '</h2>')
+ document.write('<h4>' + second + '</h4>')
+ let third = prompt('can you tell us why do you like '+second)
+ alert('your answer '+third)
+ console.log(third);
+ document.write('<h2>' + 'can you tell us why do you like' + '</h2>')
+ document.write('<h4>' + third + '</h4>')
+ let fourth = prompt('thnx for your feedback,Is there any advice from you that can help us with')
+ alert('your answer '+ fourth)
+ console.log(fourth);
+ document.write('<h2>' + 'thnx for your feedback,Is there any advice from you that can help us with' + '</h2>')
+ document.write('<h4>' + fourth + '</h4>')
+
+
